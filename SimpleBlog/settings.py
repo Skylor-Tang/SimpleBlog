845651de785a2e315f0b09014395e06d7f7f3112
skylor <739230854@qq.com>
2020-03-26 22:56:25 +0800

    static_url setting2

--- SimpleBlog/settings.py
+++ SimpleBlog/settings.py
@@ -134,7 +134,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # 全局静态文件配置
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static'),
