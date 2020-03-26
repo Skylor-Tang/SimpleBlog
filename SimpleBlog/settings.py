59b2045d4cc28fbcc969073a68758525aea494f2
skylor <739230854@qq.com>
2020-03-26 23:29:02 +0800

    v2.1

--- SimpleBlog/settings.py
+++ SimpleBlog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '359*@gk^((uteyx6%1yh#m#bmpiufutqy*!0+n5u#3&bezmf7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mydjangoblogapptmj.herokuapp.com']
 
