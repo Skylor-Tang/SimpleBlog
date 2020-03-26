33e6eb015787d51ac1c577a07605d4e5f26b6e5c
skylor <739230854@qq.com>
2020-03-26 22:15:44 +0800

    Setted django_heroku

--- SimpleBlog/settings.py
+++ SimpleBlog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -152,3 +152,5 @@ PAGINATION_SETTINGS = {
     'MARGIN_PAGES_DISPLAYED': 2,
     'SHOW_FIRST_PAGE_WHEN_INVALID': True,
 }
+
+django_heroku.settings(locals())
